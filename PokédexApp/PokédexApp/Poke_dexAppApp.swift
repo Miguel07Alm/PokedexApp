7f21807910da9d883c9efe80ac3dee6d8074ed92
Miguel07Alm <miguelangelsisi098@gmail.com>
2024-11-12 17:19:18 +0100

    build: estructura del proyecto inicial

---
+++ PokédexApp/PokédexApp/Poke_dexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_dexAppApp.swift
+//  PokédexApp
+//
+//  Created by Aula03 on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Poke_dexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokédexApp/PokédexAppTests/Poke_dexAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_dexAppTests.swift
+//  PokédexAppTests
+//
+//  Created by Aula03 on 12/11/24.
+//
+
+import Testing
+@testable import Poke_dexApp
+
+struct Poke_dexAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
