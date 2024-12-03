8f5901914bf6ce8a29c67aef40e9c2dfa76de4d6
dsa069 <113684626+dsa069@users.noreply.github.com>
2024-12-03 17:08:01 +0100

    limpieza etnica de FÜTHER

--- Pokedex/Views/FooterView.swift
+++ Pokedex/Views/FooterView.swift
@@ -2,37 +2,54 @@
 import SwiftUI
 
 struct FooterView: View {
-    @State private var selectedTab = 0
-    
+    @Binding var selectedTab : Int;
     var body: some View {
         VStack {
             ZStack{
                 Image("FooterRojo").resizable().frame(height: 200)
-                #if !v2
-                    HStack(spacing: 120) {
-                        Button("", action: {print("hello")})
-                        .buttonStyle(BotonPokedex())
-                        
-                        Button("", action: {print("hello")})
-                        .buttonStyle(BotonPerfil())
-                    }.offset(CGSize(width: 0, height: 35))
-                #else
-                    HStack(spacing: 83) {
-                        Button("", action: {print("hello")})
-                        .buttonStyle(BotonPokedex())
-                        
-                        Button("", action: {print("hello")})
-                        .buttonStyle(BotonPerfilGrande())
-                        
-                        Button("", action: {print("hello")})
-                        .buttonStyle(BotonCombate())
-                    }.offset(CGSize(width: 0, height: 35))
-                #endif
+                switch selectedTab{
+                    
+                    case 0: //Registro
+                        HStack {
+                            Button("", action: {print("hello")})
+                            .buttonStyle(BotonRegistroGrande())
+                            
+                        }.offset(CGSize(width: 0, height: 35))
+                    case 1: //Average
+                        #if !v2
+                            HStack(spacing: 120) {
+                                Button("", action: {print("hello")})
+                                .buttonStyle(BotonPokedex())
+                                
+                                Button("", action: {print("hello")})
+                                .buttonStyle(BotonPerfil())
+                            }.offset(CGSize(width: 0, height: 35))
+                        #else
+                            HStack(spacing: 83) {
+                                Button("", action: {print("hello")})
+                                .buttonStyle(BotonPokedex())
+                                
+                                Button("", action: {print("hello")})
+                                .buttonStyle(BotonPerfilGrande())
+                                
+                                Button("", action: {print("hello")})
+                                .buttonStyle(BotonCombate())
+                            }.offset(CGSize(width: 0, height: 35))
+                        #endif
+                    case 2: //Combate
+                        HStack{
+                            Button("", action: {print("hello")})
+                                .buttonStyle(BotonCombateGrande())
+                        }.offset(CGSize(width: 0, height: 35))
+                    default:
+                        Text("La cagÜe")
+                }
             }
         }.ignoresSafeArea()
     }
 }
 
 #Preview{
-    FooterView()
+    @State var selectedTab = 1;
+    FooterView(selectedTab: $selectedTab)
 }
