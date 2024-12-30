debfe865d933ab3f6bc6189386d6fd97a396e614
aula3 <Aula03@AULAIMACE13.local>
2024-12-30 18:59:20 +0100

    imaginemos que atacar funciona

--- Pokedex/Views/CombateView.swift
+++ Pokedex/Views/CombateView.swift
@@ -8,7 +8,7 @@ struct CombateView: View {
     @State var movePower = 0
     
     var body: some View {
-        
+
         ZStack{
             Color(red: 0.7529411764705882, green: 0.8588235294117647, blue: 0.8588235294117647)
                 .ignoresSafeArea()
@@ -18,36 +18,49 @@ struct CombateView: View {
                 teamView(teamId: 1)
                 teamView(teamId: 2)
             }
+            
+            Button {
+                print("MAWUOINDFUHWFUWIJ")
+                atacar(teamId: 1)
+                atacar(teamId: 2)
+            } label: {
+                Text("Ti pego")
+            }.background(Color.red)
         }
+
     }
     
     
     private func atacar(teamId: Int) -> some View {
         HStack(spacing: 25) {
-            let team = pokemonTeam.getTeam(named: teamId == 1 ? "Equipo1" : "Equipo2")
-            var teamDamage = 0
-            ForEach(Array(team!.pokemons.enumerated()), id: \.0) { poke in
+            if let team = pokemonTeam.getTeam(named: teamId == 1 ? "Equipo1" : "Equipo2") {
+                let teamDamage = team.pokemons.compactMap { poke -> Int? in
+                    guard let poke = poke else { return nil }
+                    let moveName = randomMove(poke: poke)
+                    
+                    print("Movimiento: \(moveName)")
+                    print("Precisión: \(moveAcc)")
+                    print("Daño: \(movePower)")
+                    
+                    if moveAcc > Int.random(in: 0...99) {
+                        print("Movimiento: \(moveName)")
+                        print("Precisión: \(moveAcc)")
+                        print("Daño: \(movePower)")
+                        return movePower
+                    } else {
+                        print("Movimiento: \(moveName)")
+                        print("Precisión: \(moveAcc), FALLO!!!")
+                        return 0
+                    }
+                }.reduce(0, +)
                 
-                let moveName = randomMove(poke: poke)
-
-                print("Movimiento: ", moveName)
-                print("Precisión: ", moveAcc)
-                print("Daño: ", movePower)
-                
-                if(moveAcc > Int.random(in: 0...99)){
-                    teamDamage = movePower
-                    print("Movimiento: " + moveName)
-                    print("Precisión: " + moveAcc)
-                    print("Daño: " + movePower)
-                }else{
-                    print("Movimiento: " + moveName)
-                    print("Precisión: " + moveAcc, "FALLO!!!")
-                }
+                Text("Daño del equipo \(teamId): \(teamDamage)")
+            } else {
+                Text("Equipo no encontrado")
             }
-            //print ("Daño del equipo: ", teamDamage)
         }
     }
-
+    
     private func randomMove(poke : Pokemon) -> String{
         let randMove = 0
         repeat{
