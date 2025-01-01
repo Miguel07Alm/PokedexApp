cb4f4933cfb8ca99b2f0718e8131d89dda5c6173
jmg061 <jmg061@inlumine.ual.es>
2025-01-01 20:49:21 +0100

    WINNERPOV ORDENACION SEGUN DAÑO

--- Pokedex/Views/WinnerPovView.swift
+++ Pokedex/Views/WinnerPovView.swift
@@ -1,44 +1,109 @@
 import SwiftUI
 import SDWebImageSwiftUI
 
-
 struct WinnerPovView: View {
-    @ObservedObject private var pokemonTeam = PokemonTeam.shared
-    @State var teamId : Int
+    @StateObject private var pokemonTeam = PokemonTeam.shared
+    @State var teamId: Int
+    
     var body: some View {
-        HStack(spacing: 25) {
+        HStack(spacing: 20) {
             let name = teamId == 1 ? "Equipo1" : "Equipo2"
-            let team = pokemonTeam.getTeam(named: name)
-            ForEach(0..<3, id: \.self) { i in
-                WinnerPokemonDisplay(
-                    img: URL(string: team?.pokemons[i]?.sprites.other?.showdown?.frontDefault ?? "")!,
-                    dmg: team?.pokeDamage[i] ?? 0
-                )
+            if let team = pokemonTeam.getTeam(named: name) {
+                let sortedIndices = team.pokeDamage.indices.sorted { team.pokeDamage[$0] > team.pokeDamage[$1] }
+                
+                // Second highest damage (left)
+                if sortedIndices.count > 1 {
+                    WinnerPokemonDisplay(
+                        img: URL(string: team.pokemons[sortedIndices[1]]?.sprites.other?.showdown?.frontDefault ?? "")!,
+                        dmg: team.pokeDamage[sortedIndices[1]]
+                    )
+                    .frame(width: 100, height: 425)
+                }
+                
+                // Highest damage (center)
+                if !sortedIndices.isEmpty {
+                    WinnerPokemonDisplay(
+                        img: URL(string: team.pokemons[sortedIndices[0]]?.sprites.other?.showdown?.frontDefault ?? "")!,
+                        dmg: team.pokeDamage[sortedIndices[0]]
+                    )
+                    .frame(width: 150, height: 525)
+                }
+                
+                // Lowest damage (right)
+                if sortedIndices.count > 2 {
+                    WinnerPokemonDisplay(
+                        img: URL(string: team.pokemons[sortedIndices[2]]?.sprites.other?.showdown?.frontDefault ?? "")!,
+                        dmg: team.pokeDamage[sortedIndices[2]]
+                    )
+                    .frame(width: 100, height: 425)
+                }
             }
-        }.background(Color(red: 0.7529411764705882, green: 0.8588235294117647, blue: 0.8588235294117647))
-            .ignoresSafeArea()
+        }
+        .background(Color(red: 0.7529411764705882, green: 0.8588235294117647, blue: 0.8588235294117647))
+        .ignoresSafeArea()
+    }
+    
+    private func sortPokemonByDamage(team: Team) -> [(pokemon: Pokemon, damage: Int)] {
+        let pokemonWithDamage = zip(team.pokemons, team.pokeDamage)
+            .compactMap { pokemon, damage -> (Pokemon, Int)? in
+                guard let pokemon = pokemon else { return nil }
+                return (pokemon, damage)
+            }
+        return pokemonWithDamage.sorted { $0.1 > $1.1 }
     }
 }
 
-
-struct WinnerPokemonDisplay: View {
-    @State var img: URL
-    @State var dmg: Int
+struct PokemonDamageView: View {
+    let pokemon: Pokemon
+    let damage: Int
     
     var body: some View {
-        VStack{
+        VStack {
+            WebImage(url: URL(string: pokemon.sprites.other?.officialArtwork?.frontDefault ?? ""))
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .background(Color.white.opacity(0.8))
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.blue, lineWidth: 2))
+                .shadow(radius: 5)
+            
+            Text(pokemon.name.capitalized)
+                .font(.caption)
+                .fontWeight(.bold)
+            
+            Text("Daño: \(damage)")
+                .font(.caption)
+                .foregroundColor(.red)
+        }
+        .padding()
+        .background(Color.blue.opacity(0.1))
+        .cornerRadius(15)
+    }
+}
+
+struct WinnerPokemonDisplay: View {
+    let img: URL
+    let dmg: Int
+    
+    var body: some View {
+        VStack {
             WebImage(url: img)
                 .resizable()
                 .scaledToFit()
                 .cornerRadius(20)
                 .padding()
             
-            Text("\(dmg)")
-        }.frame( height: 525)
+            Text("Daño: \(dmg)")
+                .font(.headline)
+                .foregroundColor(.red)
+        }
     }
 }
 
-    
-#Preview {
-    WinnerPovView(teamId: 1)
+struct WinnerPovView_Previews: PreviewProvider {
+    static var previews: some View {
+        WinnerPovView(teamId: 1)
+    }
 }
+
