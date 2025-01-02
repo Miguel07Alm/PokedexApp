f118e40859b535e9fc9fb4c55c889e353cf45564
jfm112INSO <informaticaualjesus@gmail.com>
2025-01-02 19:32:05 +0100

    añadir filtros restantes y busqueda + mejoras UI/UX

---
+++ Pokedex/Models/PokemonRegion.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct PokemonRegion {
+    static let regions: [(name: String, icon: String)] = [
+        ("Kanto", "kanto"),
+        ("Johto", "johto"),
+        ("Hoenn", "hoenn"),
+        ("Sinnoh", "sinnoh"),
+        ("Teselia", "teselia"),
+        ("Kalos", "kalos"),
+        ("Alola", "alola"),
+        ("Galar", "galar"),
+         ("Paldea", "paldea")
+    ]
+}
